7a5a099e92079e8780e8fdbd3f0241083a32a454
Milos1988 <vojvoda1988>
2023-04-18 18:59:48 +0200

    Logo in navbar

--- src/components/NavbarComponent/NavbarComponent.jsx
+++ src/components/NavbarComponent/NavbarComponent.jsx
@@ -1,25 +1,38 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import logoImg from "../../assets/img/logo-portfolio.png";
 
 const NavbarComponent = () => {
-    return (
-        <Navbar expand="md" className="navbarAll">
-            <Container>
-                <Navbar.Brand className="logo active" href="#home">Sudimac Milos</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav ">
-                    <Nav className="ms-auto justify-content-center navLink">
-                        <Nav.Link className="mx-2 px-3 active" href="#home" >HOME</Nav.Link>
-                        <Nav.Link className="mx-2 px-3" href="#link">ABOUT</Nav.Link>
-                        <Nav.Link className="mx-2 px-3" href="#link">SKILLS</Nav.Link>
-                        <Nav.Link className="mx-2 px-3" href="#link">PORTFOLIO</Nav.Link>
-                        <Nav.Link className="mx-2 px-3" href="#link">CONTACT</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
-}
+  return (
+    <Navbar expand="md" className="navbarAll">
+      <Container>
+        <Navbar.Brand className="logo active" href="#home">
+          <img src={logoImg} alt="" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav ">
+          <Nav className="ms-auto justify-content-center navLink">
+            <Nav.Link className="mx-2 px-3 active" href="#home">
+              HOME
+            </Nav.Link>
+            <Nav.Link className="mx-2 px-3" href="#link">
+              ABOUT
+            </Nav.Link>
+            <Nav.Link className="mx-2 px-3" href="#link">
+              SKILLS
+            </Nav.Link>
+            <Nav.Link className="mx-2 px-3" href="#link">
+              PORTFOLIO
+            </Nav.Link>
+            <Nav.Link className="mx-2 px-3" href="#link">
+              CONTACT
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
-export default NavbarComponent
+export default NavbarComponent;
