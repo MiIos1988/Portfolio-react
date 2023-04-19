adfd5c687bb425cff3704e1b91350d00b49fd904
Milos1988 <vojvoda1988@gmail.com>
2023-04-19 02:13:16 +0200

    Hamburger button stily

--- src/components/NavbarComponent/NavbarComponent.jsx
+++ src/components/NavbarComponent/NavbarComponent.jsx
@@ -2,18 +2,19 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import logoImg from "../../assets/img/logo-portfolio.png";
+import DayNightComponent from "./component/DayNightcomponent";
 
 const NavbarComponent = () => {
   return (
     <Navbar expand="md" className="navbarAll">
       <Container>
-        <Navbar.Brand className="logo active" href="#home">
+        <Navbar.Brand className="logo " href="#home">
           <img src={logoImg} alt="" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav ">
           <Nav className="ms-auto justify-content-center navLink">
-            <Nav.Link className="mx-2 px-3 active" href="#home">
+            <Nav.Link className="mx-2 px-3 " href="#home">
               HOME
             </Nav.Link>
             <Nav.Link className="mx-2 px-3" href="#link">
@@ -30,6 +31,7 @@ const NavbarComponent = () => {
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
+        <DayNightComponent/>
       </Container>
     </Navbar>
   );
