4cab5df27592c2be5f6cb349133f770075814ba8
Milos1988 <vojvoda1988@gmail.com>
2023-04-20 01:46:04 +0200

    Start working skills

--- src/components/SkillsComponent/SkillsComponent.jsx
+++ src/components/SkillsComponent/SkillsComponent.jsx
@@ -1,10 +1,21 @@
-
-
 const SkillsComponent = () => {
+  return (
+    <div className="container skills">
+      <h2>Skills & Technologies</h2>
+      <p>
+        Below you can see my skills and technologies that I use every day. I am
+        trying to keep up with the newest trends in the industry and to keep
+        learning new skills and technologies as well as improving knowledge in
+        existing ones.
+      </p>
+      <div className="iconSkills">
+        <div className="skillSection">
+            <div className="icon"></div>
+            <div className="title"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-    return(
-        <h1>Work</h1>
-    )
-}
-
-export default SkillsComponent
+export default SkillsComponent;
