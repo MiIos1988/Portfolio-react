b8b7825466247a9b45def32cdf23ebd7539482b2
Milos1988 <vojvoda1988@gmail.com>
2023-04-20 12:13:06 +0200

    Create dataSkill and finished skill section

---
+++ src/constants/icon.js
@@ -0,0 +1,75 @@
+import imgHtml from "../assets/img/icon/html.png"
+import imgCss from "../assets/img/icon/css.png"
+import imgGit from "../assets/img/icon/git.png"
+import imgJavascript from "../assets/img/icon/javascript.png"
+import imgReact from "../assets/img/icon/react.png"
+import imgSass from "../assets/img/icon/sass.png"
+import imgNode from "../assets/img/icon/node.png"
+import imgExpress from "../assets/img/icon/express.png"
+import imgMongo from "../assets/img/icon/mongoDb.png"
+import imgStripe from "../assets/img/icon/stripe.png"
+import imgRedux from "../assets/img/icon/redux.png"
+import imgBootstrap from "../assets/img/icon/bootstrap.png"
+import imgPostman from "../assets/img/icon/postman.webp"
+import imgGitHub from "../assets/img/icon/gitHub.png"
+
+const skillsData = [
+    {
+        image: imgHtml,
+        title: "HTML5"
+    },
+    {
+        image: imgCss,
+        title: "CSS3"
+    },
+    {
+        image: imgGit,
+        title: "Git"
+    },
+    {
+        image: imgJavascript,
+        title: "JavaScript"
+    },
+    {
+        image: imgReact,
+        title: "React JS"
+    },
+    {
+        image: imgSass,
+        title: "SASS"
+    },
+    {
+        image: imgNode,
+        title: "Node JS"
+    },
+    {
+        image: imgExpress,
+        title: "Express JS"
+    },
+    {
+        image: imgMongo,
+        title: "Mongo DB"
+    },
+    {
+        image: imgStripe,
+        title: "Stripe"
+    },
+    {
+        image: imgRedux,
+        title: "Redux"
+    },
+    {
+        image: imgBootstrap,
+        title: "Bootstrap"
+    },
+    {
+        image: imgPostman,
+        title: "Postman"
+    },
+    {
+        image: imgGitHub,
+        title: "GitHub"
+    },
+]
+
+export default skillsData;
