2f04c752e5eaaff3804e7b3676a7f35dab3d77f7
Milos1988 <vojvoda1988>
2023-04-23 23:05:47 +0200

    Stily project div

--- src/components/NavbarComponent/component/DayNightcomponent.jsx
+++ src/components/NavbarComponent/component/DayNightcomponent.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import Toggle from 'react-toggle';
-import 'react-toggle/style.css';
+import React, { useEffect, useState } from "react";
+import Toggle from "react-toggle";
+import "react-toggle/style.css";
 
 const DayNightComponent = () => {
   const [isChecked, setIsChecked] = useState(true);
@@ -13,30 +13,34 @@ const DayNightComponent = () => {
 
   const handleChange = () => {
     setIsChecked(!isChecked);
-    setBackground(isChecked ? "#e6e7ee " : "#212529")
-    setColor(isChecked ? "#212529" : "#e6e7ee")
-    setColorHello(isChecked ? "#212529" : "#ffe600")
-    setFilterInvert(!isChecked ? 100 : 0)
-    setShadowOne(isChecked ? "#c7c7c7" : "black")
-    setShadowTwo(isChecked ? "#fff" : "rgba(155, 155, 155, 0.5)")
-    document.documentElement.style.setProperty('--text-color', color);
-    document.documentElement.style.setProperty('--text-color-hello', colorHello);
-    document.documentElement.style.setProperty('--background-color', background);
-    document.documentElement.style.setProperty('--filter-invert', filterInvert);
-    document.documentElement.style.setProperty('--shadow-one', shadowOne);
-    document.documentElement.style.setProperty('--shadow-two', shadowTwo);
-
+    setBackground(isChecked ? "#e6e7ee " : "#212529");
+    setColor(isChecked ? "#212529" : "#e6e7ee");
+    setColorHello(isChecked ? "#212529" : "#ffe600");
+    setFilterInvert(!isChecked ? 100 : 0);
+    setShadowOne(isChecked ? "#c7c7c7" : "black");
+    setShadowTwo(isChecked ? "#fff" : "rgba(155, 155, 155, 0.5)");
+    document.documentElement.style.setProperty("--text-color", color);
+    document.documentElement.style.setProperty(
+      "--text-color-hello",
+      colorHello
+    );
+    document.documentElement.style.setProperty(
+      "--background-color",
+      background
+    );
+    document.documentElement.style.setProperty("--filter-invert", filterInvert);
+    document.documentElement.style.setProperty("--shadow-one", shadowOne);
+    document.documentElement.style.setProperty("--shadow-two", shadowTwo);
   };
 
-
   return (
-    <div className='dayNight'>
+    <div className="dayNight">
       <Toggle
         checked={isChecked}
         onChange={handleChange}
         icons={{
-          checked: <span className='day'>🌞</span>,
-          unchecked: <span className='night'>🌙</span>,
+          checked: <span className="day">🌙</span>,
+          unchecked: <span className="night">🌞</span>,
         }}
         aria-label="Toggle theme"
       />
--- src/components/ProjectComponent/component/OneProjectComponent.jsx
+++ src/components/ProjectComponent/component/OneProjectComponent.jsx
@@ -1,32 +1,35 @@
 import git from "../../../assets/img/icon/githubProject.webp";
+import Tilt from "react-parallax-tilt";
 
 const OneProjectComponent = ({ data }) => {
   const { image, title, desc, tags, github, live } = data;
 
   return (
-    <div className="product p-5 mt-5 d-flex flex-column">
-      <a href={live} target="_blank">
-        <img className="prImg" src={image} alt="" />
-      </a>
+    
+      <Tilt className="product p-5 mt-5 d-flex flex-column">
+        <a href={live} target="_blank">
+          <img className="prImg" src={image} alt="" />
+        </a>
 
-      <h2 className="title">{title}</h2>
+        <h2 className="title">{title}</h2>
 
-      <p className="desc">{desc}</p>
-      <div className="blockTags mt-auto">
-        <div className=" d-flex flex-wrap gap-3">
-          {tags.map((el, index) => {
-            return (
-              <div key={index} className="tags p-2">
-                {el}
-              </div>
-            );
-          })}
+        <p className="desc">{desc}</p>
+        <div className="blockTags mt-auto">
+          <div className=" d-flex flex-wrap gap-3">
+            {tags.map((el, index) => {
+              return (
+                <div key={index} className="tags p-2">
+                  {el}
+                </div>
+              );
+            })}
+          </div>
         </div>
-      </div>
-      <a href={github} target="_blank" className="git">
-        <img src={git} />
-      </a>
-    </div>
+        <a href={github} target="_blank" className="git">
+          <img src={git} />
+        </a>
+      </Tilt>
+    
   );
 };
 
