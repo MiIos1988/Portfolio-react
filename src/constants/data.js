0d31dd34e23ad59342d456a142610bbf73cd5e99
Milos1988 <vojvoda1988@gmail.com>
2023-04-25 10:31:52 +0200

    Add description in data.js

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,4 +1,15 @@
 import img1 from "../assets/img/projectImg/shop.png";
+import img2 from "../assets/img/projectImg/bank-app.png";
+import img3 from "../assets/img/projectImg/blog-post.png";
+import img4 from "../assets/img/projectImg/chess.png";
+import img5 from "../assets/img/projectImg/calculator.png";
+import img6 from "../assets/img/projectImg/stopwatch.png";
+import img7 from "../assets/img/projectImg/quiz.png";
+import img8 from "../assets/img/projectImg/rock-paper-scissors.png";
+import img9 from "../assets/img/projectImg/clock.png";
+import img10 from "../assets/img/projectImg/hangman.png";
+import img11 from "../assets/img/projectImg/tic-tac-toe.png";
+import img12 from "../assets/img/projectImg/slider.png";
 
 const data = [
   {
@@ -10,28 +21,92 @@ const data = [
     live: "https://frontend-my-shop.vercel.app/",
   },
   {
-    image: img1,
-    title: "Zurea shop",
-    desc: "Full featured e-commerce store builts.",
-    tags: ["React", "Redux", "NodeJs", "MongoDB"],
-    github: "https://github.com/VojvodaMilos/My_shop",
-    live: "https://frontend-my-shop.vercel.app/",
+    image: img2,
+    title: "Bank app",
+    desc: "Entering user data and records of entered data that the user can modify or delete.",
+    tags: ["React"],
+    github: "https://github.com/VojvodaMilos/bank-app",
+    live: "https://bank-app-lovat.vercel.app/",
   },
   {
-    image: img1,
-    title: "Zurea shop",
-    desc: "Full featured e-commedadasdas asdasdasdas dasdasdasd asdasdasda asdasdasd asdasdasdas adasdasdas asdasdasds asdasdasdasd asdasrce store built with MERN stack and Paypal payments API. Admin page to manage customers, products & orders.",
-    tags: ["React", "Redux", "NodeJs", "MongoDB", "JWT", "Express"],
-    github: "https://github.com/VojvodaMilos/My_shop",
-    live: "https://frontend-my-shop.vercel.app/",
+    image: img3,
+    title: "Blog post app",
+    desc: "Choose a category and the application will show you all the posts from the selected category. Also, within a post, you can choose a category.",
+    tags: ["React"],
+    github: "https://github.com/VojvodaMilos/post-app-react",
+    live: "https://post-app-react-phi.vercel.app/",
   },
   {
-    image: img1,
-    title: "Zurea shop",
-    desc: "Full featured e-commerce store built with MERN stack and Paypal payments API. Admin page to manage customers, products & orders.",
-    tags: ["React", "Redux", "NodeJs", "MongoDB", "JWT", "Express"],
-    github: "https://github.com/VojvodaMilos/My_shop",
-    live: "https://frontend-my-shop.vercel.app/",
+    image: img6,
+    title: "Stopwatch",
+    desc: "The stopwatch features time measurement, pause and  reset. This project allowed me to practice manipulating component state in React.js and improving my overall React.js development skills.",
+    tags: ["React"],
+    github: "https://github.com/VojvodaMilos/stopwatch",
+    live: "https://stopwatch-five-beta.vercel.app/",
+  },
+  {
+    image: img7,
+    title: "Quiz",
+    desc: " The quiz is designed to test users knowledge on a specific topic and features multiple-choice questions and a final score display.",
+    tags: ["React"],
+    github: "https://github.com/VojvodaMilos/quiz",
+    live: "https://quiz-livid-chi.vercel.app/",
+  },
+  {
+    image: img8,
+    title: "Rock paper scissors",
+    desc: "The game allows users to play against the computer, selecting their choice of rock, paper, or scissors.  Additionally, the game keeps track of the user's score and displays it at the end of each round. ",
+    tags: ["React"],
+    github: "https://github.com/VojvodaMilos/rock-paper-scissors",
+    live: "https://rock-paper-scissors-ten-pi.vercel.app/",
+  },
+  {
+    image: img4,
+    title: "Chess - movement of pieces",
+    desc: "The application allows users to move chess pieces on a virtual board according to the rules of the game. Additionally, the application includes a feature that highlights the possible moves for each piece, helping users learn the game and make informed decisions.",
+    tags: ["Javascript"],
+    github: "https://github.com/VojvodaMilos/Chess",
+    live: "https://chess-six-sage.vercel.app/",
+  },
+  {
+    image: img5,
+    title: "Calculator",
+    desc: "Calculator App with vanilla JS. Simple app with basic arithmetic operations.",
+    tags: ["Javascript"],
+    github: "https://github.com/VojvodaMilos/calculator",
+    live: "https://calculate-navy.vercel.app/",
+  },
+  {
+    image: img9,
+    title: "Analog clock",
+    desc: "This application is an analog clock that displays the current time in a format similar to a classic analog clock. It allows users to track the exact time in real time. ",
+    tags: ["Javascript"],
+    github: "https://github.com/VojvodaMilos/analogClock",
+    live: "https://analog-clock-nu.vercel.app/",
+  },
+  {
+    image: img10,
+    title: "Hangman game",
+    desc: "The game is a classic word-guessing game where the player tries to guess a hidden word by guessing one letter at a time. This project allowed me to further develop my skills in JavaScript.",
+    tags: ["Javascript"],
+    github: "https://github.com/VojvodaMilos/Hangman_game",
+    live: "https://hangman-game-liart-one.vercel.app/",
+  },
+  {
+    image: img11,
+    title: "Tic-Tac-Toe Artificial intelligence",
+    desc: "Play Tic-Tac-Toe game against the computer and try to beat the artificial intelligence.",
+    tags: ["Javascript"],
+    github: "https://github.com/VojvodaMilos/Tic-Tac-Toe-artificial-intelligence",
+    live: "https://new-tic-tac-toe-chi.vercel.app/",
+  },
+  {
+    image: img12,
+    title: "Gallery",
+    desc: "As part of my portfolio, I created an gallery using HTML, CSS, and JavaScript. The gallery features a responsive layout with arrow buttons that allow users to cycle through the images.",
+    tags: ["Javascript"],
+    github: "https://github.com/VojvodaMilos/background-slider",
+    live: "https://background-slider-opal.vercel.app/",
   },
 ];
 
