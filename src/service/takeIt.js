fc4a3238356c593709c78b68fceefe42ae2355be
Milos1988 <vojvoda1988@gmail.com>
2023-05-05 22:52:34 +0200

    Resolved conflict in src/service/takeIt.js

--- src/service/takeIt.js
+++ src/service/takeIt.js
@@ -5,6 +5,9 @@ const takeIt = () => {
             console.log(data.ip)
             return fetch("http://localhost:5050/api/authentication", {
                 method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                  },
                 body: JSON.stringify({ip: data.ip})
             })
 
