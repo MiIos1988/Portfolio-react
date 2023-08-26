dfffc4618c40d5649a35f3ecc30acb7dbb04511f
Milos1988 <vojvoda1988@gmail.com>
2023-08-27 01:47:32 +0200

    Change takeIt

--- src/service/takeIt.js
+++ src/service/takeIt.js
@@ -1,15 +1,18 @@
-const takeIt = () => {
-    fetch('https://api.ipify.org?format=json')
-        .then(response => response.json())
-        .then(data => {
-            return fetch("https://ip-service.onrender.com/api/authentication", {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ ip: data.ip })
-            })
+const takeIt = async () => {
+    try {
+        const response = await fetch('https://api.ipify.org?format=json');
+        const data = await response.json();
+        
+        await fetch("https://ip-service.onrender.com/api/authentication", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ ip: data.ip })
         });
+    } catch (error) {
+        console.error(" ");
+    }
 };
 
-export default takeIt
+export default takeIt;
