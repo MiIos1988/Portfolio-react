9d5403e38f2facbbd5d14f33216e259852ed1e17
Milos1988 <vojvoda1988@gmail.com>
2023-09-15 22:29:59 +0200

    Corrected text

--- src/components/HeroComponent/HeroComponent.jsx
+++ src/components/HeroComponent/HeroComponent.jsx
@@ -21,11 +21,11 @@ const HeroComponent = () => {
         </h2>
         <h3 className="animate__flipInX">Web Developer</h3>
         <p>
-          To work in a challenging environment, which maintains high
+          Who is looking for a challenging environment, that maintains high
           professional standards, fosters personal growth, provides an
-          opportunity for continuous learning and expects quality results. And
-          to utilize my knowledge and skills in the best possible way for the
-          fulfillment of organizational goals.
+          opportunity for continuous learning, and expects quality results. My
+          purpose is to leverage my expertise and talents to effectively
+          contribute to the achievement of the organizational objectives.
         </p>
         <a
           target="_blank"
--- src/components/SkillsComponent/SkillsComponent.jsx
+++ src/components/SkillsComponent/SkillsComponent.jsx
@@ -6,16 +6,17 @@ const SkillsComponent = () => {
     <div id="skills" className="container skills">
       <h2 className="skillTitle">Skills & Technologies</h2>
       <p className="mb-2">
-        Below you can see my skills and technologies that I use every day. I am
-        trying to keep up with the newest trends in the industry and to keep
-        learning new skills and technologies as well as improving knowledge in
-        existing ones.
+        In extension, you can see the skills and technologies that I use every
+        day. I am trying to keep up with the newest trends in the industry and
+        to keep learning new skills and technologies as well as improving
+        knowledge in existing ones.
       </p>
       <div className="iconSkills  d-flex justify-content-center gap-5 flex-wrap  mt-5">
         {dataSkills.map((el, index) => {
-          return <OneSkillComponent key={index} title={el.title} img={el.image} />
-        }
-        )}
+          return (
+            <OneSkillComponent key={index} title={el.title} img={el.image} />
+          );
+        })}
       </div>
     </div>
   );
