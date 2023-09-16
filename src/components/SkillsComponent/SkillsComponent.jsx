75a09a4a219c44f03f8f9bf9603571a6b74a7294
Milos1988 <vojvoda1988>
2023-09-16 19:23:40 +0200

    Corected text

--- src/components/SkillsComponent/SkillsComponent.jsx
+++ src/components/SkillsComponent/SkillsComponent.jsx
@@ -6,10 +6,11 @@ const SkillsComponent = () => {
     <div id="skills" className="container skills">
       <h2 className="skillTitle">Skills & Technologies</h2>
       <p className="mb-2">
-        In extension, you can see the skills and technologies that I use every
-        day. I am trying to keep up with the newest trends in the industry and
-        to keep learning new skills and technologies as well as improving
-        knowledge in existing ones.
+        As following, you may see the skills and technologies that I use on
+        daily basis. This is just beginning & start-up and accordingly I
+        continue to keep up with the newest trends in the industry and to keep
+        learning new skills and technologies as well as improving knowledge in
+        existing ones.
       </p>
       <div className="iconSkills  d-flex justify-content-center gap-5 flex-wrap  mt-5">
         {dataSkills.map((el, index) => {
