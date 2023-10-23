c18a16fef91b1a22b9568afb45317d7c05515edb
Milos1988 <vojvoda1988@gmail.com>
2023-10-23 23:37:16 +0200

    Add socket.io

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useEffect } from "react";
 import HomePageComponent from "./pages/HomePageComponent";
 import takeIt from "./service/takeIt";
+import io from "socket.io-client";
+
+const socket = io.connect("http://localhost:5500");
 
 function App() {
   useEffect(() => {
