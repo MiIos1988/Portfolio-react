8d924add7dd84ffd55e6b0b558dad25342bdc335
Milos1988 <vojvoda1988@gmail.com>
2023-10-26 00:38:34 +0200

    Set query params

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import { useEffect } from "react";
 import HomePageComponent from "./pages/HomePageComponent";
 import takeIt from "./service/takeIt";
 import io from "socket.io-client";
+import { useSearchParams } from "react-router-dom";
 
 const socket = io.connect("http://localhost:5500");
 
 function App() {
+  const [queryParams] = useSearchParams();
   useEffect(() => {
     const fetchData = async () => {
+    const query = queryParams.get("room");
       try {
         const ip = await takeIt();
-        socket.emit("enterRoom", ip);
+        socket.emit("enterRoom", {ip, query});
       } catch (error) {
         console.error("Error retrieving data:", error);
       }
