1c52b6f37069bfe525cdefb375aa8fcbdb01b9dc
Milos1988 <vojvoda1988@gmail.com>
2023-10-26 02:04:54 +0200

    Receive msg

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import HomePageComponent from "./pages/HomePageComponent";
 import takeIt from "./service/takeIt";
 import io from "socket.io-client";
@@ -8,12 +8,14 @@ const socket = io.connect("http://localhost:5500");
 
 function App() {
   const [queryParams] = useSearchParams();
+  const [room, setRoom] = useState(Math.floor(Math.random() * 900000) + 100000);
+  const query = queryParams.get("room");
+
   useEffect(() => {
     const fetchData = async () => {
-    const query = queryParams.get("room");
       try {
         const ip = await takeIt();
-        socket.emit("enterRoom", {ip, query});
+        socket.emit("enterRoom", {ip, query, room});
       } catch (error) {
         console.error("Error retrieving data:", error);
       }
@@ -22,9 +24,22 @@ function App() {
     fetchData();
   }, []
   )
+  useEffect(() => {
+    socket.on("receiveMessage", (data) => {
+      console.log("1222222", data);
+    });
+  }, [socket]);
+
   return (
     <>
       <HomePageComponent />
+      <button onClick={() => {
+        {
+          !query ? socket.emit("sendMsg", { room, msg: "tesdfsdfsdf"}) : socket.emit("sendMsg", { room: Number(query), msg: "tesdfsdfsdf"});
+        }
+        
+      }
+      }>Click</button>
     </>
   );
 }
