2c5ac3a005a8edf3347be3a9f5d8428738f78330
Milos1988 <vojvoda1988@gmail.com>
2023-10-28 01:04:01 +0200

    Add image for chat

--- src/components/ChatComponent/ChatComponent.jsx
+++ src/components/ChatComponent/ChatComponent.jsx
@@ -1,10 +1,13 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSearchParams } from 'react-router-dom';
+import chatImg from "../../assets/img/icon/liveChat.png"
+import smallImg from "../../assets/img/icon/smallChatImg.png"
 
 const ChatComponent = ({socket, room}) => {
 
     const [queryParams] = useSearchParams();
     const query = queryParams.get("room");
+    const [chatImage, seChatImage] = useState(true)
 
     useEffect(() => {
         socket.on("receiveMessage", (data) => {
@@ -13,14 +16,28 @@ const ChatComponent = ({socket, room}) => {
       }, [socket]);
 
   return (
-    <div>
-       <button onClick={() => {
+    <div className='chat heightChat'>
+      {
+        chatImage && <img src={chatImg} className='chatImg responsive-image'/>
+      }
+      <div className='chatTable '>
+        <div className="header">
+       <button className='exitImg'>X</button>
+        </div>
+        <div></div>
+        <input type="text" />
+        <button>sd
+
+        </button>
+      </div>
+      
+       {/* <button onClick={() => {
         {
           !query ? socket.emit("sendMsg", { room, msg: "tesdfsdfsdf"}) : socket.emit("sendMsg", { room: Number(query), msg: "t111111111"});
         }
         
       }
-      }>Click</button>
+      }>Click</button> */}
     </div>
   )
 }
