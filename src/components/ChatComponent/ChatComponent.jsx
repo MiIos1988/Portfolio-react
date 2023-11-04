bf6bc5f0685b404c919795bf048c52458ce6e740
Milos1988 <vojvoda1988@gmail.com>
2023-11-04 01:30:47 +0100

    Show i hide chat

--- src/components/ChatComponent/ChatComponent.jsx
+++ src/components/ChatComponent/ChatComponent.jsx
@@ -11,6 +11,8 @@ const ChatComponent = ({ socket, room }) => {
   const [chatImage, seChatImage] = useState(true);
   const [currentMessage, setCurrentMessage] = useState("");
   const [messageList, setMessageList] = useState([]);
+  const [show, setShow] = useState(false);
+  const [showImgChat, setShowImgChat] = useState(false);
   const inputRef = useRef();
 
   useEffect(() => {
@@ -21,55 +23,70 @@ const ChatComponent = ({ socket, room }) => {
 
   useEffect(() => {
     inputRef.current.focus();
-  }, [messageList]
-  )
+  }, [messageList]);
 
   const sendMessage = () => {
     !query
-              ? socket.emit("sendMsg", { room, msg: currentMessage, author: "client" })
-              : socket.emit("sendMsg", {
-                  room: Number(query),
-                  msg: currentMessage,
-                  author: "admin" 
-                });
-            setMessageList((list) => [...list, query ? {msg: currentMessage, author: "admin"} : {msg: currentMessage, author: "client"}]);
-            setCurrentMessage("");
-  }
+      ? socket.emit("sendMsg", { room, msg: currentMessage, author: "client" })
+      : socket.emit("sendMsg", {
+          room: Number(query),
+          msg: currentMessage,
+          author: "admin",
+        });
+    setMessageList((list) => [
+      ...list,
+      query
+        ? { msg: currentMessage, author: "admin" }
+        : { msg: currentMessage, author: "client" },
+    ]);
+    setCurrentMessage("");
+  };
 
   return (
-    <div className="chat  ">
-      {chatImage && <img src={chatImg} className="chatImg responsive-image" />}
-      <div className="chatTable responsive-chat">
+    <div className="chat ">
+      {chatImage && (
+        <img
+          src={chatImg}
+          className={`chatImg responsive-image ${
+            showImgChat ? "show" : "hide"
+          }`}
+          onClick={() => setShow((prev) => !prev)}
+        />
+      )}
+      <div className={`chatTable responsive-chat ${show ? "show" : "hide"}`}>
         <div className="header">
-          <p>Live Chat</p>
-          <button className="exitImg">X</button>
+          <p>Live Chat with Milos</p>
+          <button className="exitImg" onClick={() => setShow((prev) => !prev)}>
+            X
+          </button>
         </div>
         <ScrollToBottom className="chatBody">
           {messageList.map((message, i) => {
             return (
-              <div key={i} className={ message.author === "admin" ? "admin " : "client"}>
+              <div
+                key={i}
+                className={message.author === "admin" ? "admin " : "client"}
+              >
                 <p>{message.msg}</p>
               </div>
             );
           })}
         </ScrollToBottom>
         <div className="inputAndBtn">
-        <input
-          className="inputField"
-          ref={inputRef}
-          type="text"
-          value={currentMessage}
-          placeholder="Write a message and press enter..."
-          onChange={(e) => setCurrentMessage(e.target.value)}
-          onKeyPress={(event) => {
-            event.key === "Enter" && sendMessage();
-          }}
-        />
-        <button
-          onClick={() => sendMessage()}
-        >
-           <BsFillSendFill/>
-        </button>
+          <input
+            className="inputField"
+            ref={inputRef}
+            type="text"
+            value={currentMessage}
+            placeholder="Write a message and press enter..."
+            onChange={(e) => setCurrentMessage(e.target.value)}
+            onKeyPress={(event) => {
+              event.key === "Enter" && sendMessage();
+            }}
+          />
+          <button onClick={() => sendMessage()}>
+            <BsFillSendFill />
+          </button>
         </div>
       </div>
     </div>
