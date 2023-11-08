ca3bb723f1a73656f67539c464273e395b6a70a4
Milos1988 <vojvoda1988@gmail.com>
2023-11-08 23:18:02 +0100

    Add render server

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { useSearchParams } from "react-router-dom";
 import ChatComponent from "./components/ChatComponent/ChatComponent";
 import axios from "axios";
 
-const socket = io.connect("http://localhost:5500");
+const socket = io.connect("https://chat-server-56gq.onrender.com");
 
 function App() {
   
